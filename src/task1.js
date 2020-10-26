84c475acf289b7aaf73f046bbca1aed96d88e4d4
Andrii Pysmennyi <andrey.ad511989@gmail.com>
2020-10-26 08:43:15 +0200

    task 1

--- src/task1.js
+++ src/task1.js
@@ -1,5 +1,11 @@
 const delay = ms => {
   // Твой код
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(ms)
+    }, ms)
+})
+
 };
 
 const logger = time => console.log(`Resolved after ${time}ms`);
